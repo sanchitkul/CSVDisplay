22fd948d281d60543423d36e273d51d963677096
Your Name <you@example.com>
2017-10-18 00:35:27 -0400

    Created Form Page in homepage class

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<?php
+class homepage extends page {
+
+    public function get()
+    {
+        $form = '<form method="post" enctype="multipart/form-data">';
+        $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
+        $form .= '<input type="submit" value="Upload Image" name="submit">';
+        $form .= '</form> ';
+        $this->html .= '<h1>Upload Form</h1>';
+        $this->html .= $form;
+
+    }
+
+    
+?>
