82bd2f3e8de13c1e4c10620afcab678823c184ff
Your Name <you@example.com>
2017-10-18 00:30:15 -0400

    Added string functions

---
+++ stringFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+class stringFunctions {
+     static public function printThis($inputText) {
+        return print($inputText);
+     }
+     static public function stringLength($text) {
+        return strlen($text);
+     }  
+  }
+?>
