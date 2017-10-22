4ffd56c4ca61a5fe02b82207105b098e77d2afbc
Your Name <you@example.com>
2017-10-21 21:18:05 -0400

    Added program for rows display

--- htmlTable.php
+++ htmlTable.php
@@ -1,25 +1,53 @@
 <?php
 class htmlTable extends page {
-    public function getmethod()
+    public function get()
     {
         $row=1;
-        if (($$table= fopen($_GET['filename'],"r")) ! == false )
+        if (($$table= fopen($_GET['filename'],"r")) !== false )
         {   
             echo '<table border="1">';
-            while (($data = fgetcsv($table,1000, "," )) ! == FALSE)
+            while (($data = fgetcsv($table,1000, "," )) !== FALSE)
             {   
-                $num=count($data);
+                $num = count($data);
                 if ($row == 1)
                 {
-                    echo '<head><tr>';
+                    echo '<h1>Table</h1>';
+                    echo '<thead><tr>';
                 }
                 else
                 {
                     echo '<tr>';
                 }
+                for ($a = 0 ; $a < $num ; $a++)
+                {
+                 if (empty($data[$a]))
+                 {
+                    $value = "&nbsp;";
                 }
+                else
+                {
+                    $value = $data[$a];
+
+                }
+                if ($row == 1) {
+                echo '<th>'.$value.'</th>';
+            }else{
+                echo '<td>'.$value.'</td>';
             }
         }
-    }
-}
-?>
+
+                if ($row ==1)
+                {
+                    echo '</tr></thead><tbody>';
+                }
+                else
+                    {
+                        echo '</tr>';
+                     }
+                     $row++;
+                 }
+                 echo '</tbody></table>';
+                 fclose ($table);
+             }
+         }
+        }
