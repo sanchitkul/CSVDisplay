a114dba78812272f35c049589bf9a6d786560824
Your Name <you@example.com>
2017-10-21 21:20:25 -0400

    Added statics

--- homepage.php
+++ homepage.php
@@ -1,16 +1,23 @@
 <?php
 class homepage extends page {
 
-    public function get()
+    static public function get()
     {
         $form = '<form method="post" enctype="multipart/form-data">';
         $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
-        $form .= '<input type="submit" value="Upload Image" name="submit">';
+        $form .= '<input type="submit" value="Upload and Display CSV File" name="submit">';
         $form .= '</form> ';
-        $this->html .= '<h1>Upload Form</h1>';
+        $this->html .= '<h1>Display Contents of a CSV File</h1>';
         $this->html .= $form;
 
     }
 
-    
+    static public function post() {
+        $target_dir = "uploads/";
+    	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file );
+    	header('Location: index.php?page=htmlTable&filename='. $target_file );
+
+    }
+}
 ?>
--- htmlTable.php
+++ htmlTable.php
@@ -1,53 +1,50 @@
 <?php
 class htmlTable extends page {
-    public function get()
-    {
-        $row=1;
-        if (($$table= fopen($_GET['filename'],"r")) !== false )
-        {   
-            echo '<table border="1">';
-            while (($data = fgetcsv($table,1000, "," )) !== FALSE)
-            {   
-                $num = count($data);
-                if ($row == 1)
-                {
-                    echo '<h1>Table</h1>';
-                    echo '<thead><tr>';
-                }
-                else
-                {
-                    echo '<tr>';
-                }
-                for ($a = 0 ; $a < $num ; $a++)
-                {
-                 if (empty($data[$a]))
-                 {
-                    $value = "&nbsp;";
-                }
-                else
-                {
-                    $value = $data[$a];
 
-                }
-                if ($row == 1) {
+public function get(){
+
+$row = 1;
+if (($table = fopen($_GET['filename'], "r" )) !== false ) {
+
+    echo '<table border="1">';
+
+    while (($data = fgetcsv($table, 100000, ",")) !== FALSE) {
+        $num = count($data);
+        if ($row == 1) {
+            echo '<h1> Table </h1>';
+            echo '<thead><tr><br>';
+        }else
+        {
+            echo '<tr>';
+        }
+
+        for ($a=0; $a < $num; $a++) {
+            
+            if(empty($data[$a])) {
+               $value = "&nbsp;";
+            }else{
+               $value = $data[$a];
+            }
+            if ($row == 1) {
                 echo '<th>'.$value.'</th>';
             }else{
                 echo '<td>'.$value.'</td>';
             }
         }
 
-                if ($row ==1)
-                {
-                    echo '</tr></thead><tbody>';
-                }
-                else
-                    {
-                        echo '</tr>';
-                     }
-                     $row++;
-                 }
-                 echo '</tbody></table>';
-                 fclose ($table);
-             }
-         }
+        if ($row == 1) {
+            echo '</tr></thead><tbody>';
+        }else{
+            echo '</tr>';
         }
+        $row++;
+    }
+
+    echo '</tbody></table>';
+    fclose($table);
+}
+
+  }
+
+}
+?>
